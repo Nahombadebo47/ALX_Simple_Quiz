14166ef18ea6a43ac0aba56a05be4fa7dd822771
Nahombadebo47 <nahombadebo47@gmail.com>
2024-07-19 17:07:58 +0300

    Small Modification on Javascript file

--- quiz.js
+++ quiz.js
@@ -2,12 +2,12 @@ function checkAnswer() {
     correctAnswer = '4'; 
     let userAnswer = document.querySelector('input[name="quiz"]:checked').value; 
     let feedback = document.querySelector('#feedback');
-    if(userAnswer === correctAnswer) {
+    if (userAnswer === correctAnswer) {
         feedback.textContent = 'Correct! Well done.';
     } else{
         feedback.textContent = "That's incorrect. Try again!";
     };
 }
-const subBtn = document.getElementById('submit-answer');
+const submitAnswer = document.getElementById('submit-answer');
 
-subBtn.addEventListener('click', checkAnswer); 
+submitAnswer.addEventListener('click', checkAnswer); 
